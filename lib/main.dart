d0c65977b4327d12c2e676efe59a8c6b8906b3e4
JUST1TURKI <Tsgtsg2002@gmail.com>
2023-03-09 04:12:04 +0300

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -13,15 +13,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-        designSize: Size(360, 780),
-    builder: (context , child) =>GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
-    ));
+    return Scaffold();
   }
 }
