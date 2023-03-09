3f5ab8de345c4ef12b47cd50d66731ba76b24af9
JUST1TURKI <Tsgtsg2002@gmail.com>
2023-03-09 18:58:18 +0300

    Changes

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -17,6 +17,199 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: color.AppColor.homePageBackground,
+      body: Container(
+        padding: EdgeInsets.only(top: 70, left: 30, right: 30),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(
+                  'Daily Planner',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: color.AppColor.homePageTitle,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Expanded(child: Container()),
+                Icon(
+                  Icons.arrow_back_ios,
+                  size: 20,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Icon(
+                  Icons.calendar_today_outlined,
+                  size: 20,
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Icon(
+                  Icons.arrow_forward_ios,
+                  size: 20,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                Text(
+                  'today Plan',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: color.AppColor.homePageSubtitle,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Expanded(child: Container()),
+                Text(
+                  'Details',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: color.AppColor.homePageDetail,
+                  ),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Icon(
+                  Icons.arrow_forward,
+                  size: 20,
+                  color: color.AppColor.homePageIcons,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 220,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    color.AppColor.gradientFirst,
+                    color.AppColor.gradientFirst.withOpacity(0.8),
+                    color.AppColor.gradientSecond,
+                  ],
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.topRight,
+                ),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                  topRight: Radius.circular(80),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(7, 10),
+                    blurRadius: 15,
+                    color: color.AppColor.gradientSecond,
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Clock Timer',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        color: color.AppColor.homePageContainerTextSmall,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Startup & Boost your Productivity  ',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w700,
+                        color: color.AppColor.homePageContainerTextSmall,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.local_fire_department_rounded,
+                          size: 30,
+                          color: color.AppColor.fire,
+                        ),
+                        Icon(
+                          Icons.local_fire_department_rounded,
+                          size: 30,
+                          color: color.AppColor.fire,
+                        ),
+                        Icon(
+                          Icons.local_fire_department_rounded,
+                          size: 30,
+                          color: color.AppColor.fire,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.timer_outlined,
+                              size: 20,
+                              color: color.AppColor.homePageContainerTextSmall,
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              '45 min',
+                              style: TextStyle(
+                                fontSize: 15,
+                                color:
+                                    color.AppColor.homePageContainerTextSmall,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Expanded(child: Container()),
+                        Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(60),
+                              boxShadow: [
+                                BoxShadow(
+                                    offset: Offset(4, 10),
+                                    blurRadius: 10,
+                                    color: color.AppColor.gradientFirst)
+                              ]),
+                          child: Icon(
+                            Icons.play_circle_fill,
+                            color: Colors.white,
+                            size: 60,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,13 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Workout Gym',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
+    );
   }
 }
