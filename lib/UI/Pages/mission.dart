af957f0efbc87d945d00b6f436cb453e246bf561
JUST1TURKI <Tsgtsg2002@gmail.com>
2023-04-03 15:43:09 +0300

    Changes

--- lib/UI/Pages/mission.dart
+++ lib/UI/Pages/mission.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:video_player_app/UI/widgets/button.dart';
+import 'package:video_player_app/controller/task_controller.dart';
 import '../colors.dart' as color;
 import 'addTaskPage.dart';
 
@@ -11,6 +13,7 @@ class Missions extends StatefulWidget {
 }
 
 class _Missions extends State<Missions> {
+  final TaskController _taskController = Get.put(TaskController());
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -31,34 +34,59 @@ class _Missions extends State<Missions> {
               borderRadius: const BorderRadius.only(
                   topLeft: Radius.circular(60), topRight: Radius.circular(60))),
           child: Column(
+            children: [_AddTask()],
+          ),
+        ));
+  }
+
+// ! Add Task
+  Padding _AddTask() {
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Row(
+        children: [
+          Column(
             children: [
-              Padding(
-                padding: const EdgeInsets.all(25.0),
-                child: Row(
-                  children: [
-                    const Text(
-                      'Daily Missions',
-                      style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
+              const Text(
+                'Daily Missions',
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              Row(
+                children: [
+                  Text(
+                    DateFormat.EEEE().format(DateTime.now()),
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
                     ),
-                    const SizedBox(
-                      width: 50,
-                    ),
-                    Expanded(
-                      child: MyButton(
-                        label: 'add Task',
-                        onTap: () {
-                          Get.to(const AddTaskPage());
-                        },
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    DateFormat.yMMMd().format(DateTime.now()),
+                    style: const TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ],
               )
             ],
           ),
-        ));
+          const SizedBox(
+            width: 50,
+          ),
+          Expanded(
+            child: MyButton(
+              label: 'add Task',
+              onTap: () {
+                Get.to(const AddTaskPage());
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
