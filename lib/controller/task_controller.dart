a419a49f219dd933ac2c6ccddfc6ddfc9093bbc0
JUST1TURKI <Tsgtsg2002@gmail.com>
2023-04-12 23:20:57 +0300

    Changes

--- lib/controller/task_controller.dart
+++ lib/controller/task_controller.dart
@@ -1,28 +1,33 @@
 import 'package:get/get.dart';
-
-import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:video_player_app/db/db_helper.dart';
 
 import '../models/task.dart';
 
 class TaskController extends GetxController {
-  final taskList = <Task>[
-    Task(
-      title: 'Title1',
-      note: 'Note Something',
-      startTime: '2:16AM',
-      endTime: '2:18PM',
-      color: 0,
-      isCompleted: 0,
-    ),
-    Task(
-      title: 'Title2',
-      note: 'Note Something here',
-      startTime: '2:19AM',
-      endTime: '2:10PM',
-      color: 2,
-      isCompleted: 1,
-    ),
-  ];
-  addTask({Task? task}) {}
-  getTask() {}
+  final RxList<Task> taskList = <Task>[].obs;
+
+  Future<int> addTask({Task? task}) {
+    return DBHelper.insert(task);
+  }
+
+  // ? GET DATA FROM DATABASE
+  Future<void> getTask() async {
+    // ignore: unused_local_variable
+    List<Map<String, dynamic>> task = await DBHelper.query(null);
+    taskList.assignAll(task.map((data) => Task.fromJson(data)).toList());
+  }
+
+  // ? DELETE DATA FROM DATABASE
+  void deleteTask({required Task task}) async {
+    // ignore: unused_local_variable
+    await DBHelper.delete(task);
+    getTask();
+  }
+
+  // ? UPDATE DATA FROM DATABASE
+  void markTaskCompleted(int id) async {
+    // ignore: unused_local_variable
+    await DBHelper.update(id);
+    getTask();
+  }
 }
